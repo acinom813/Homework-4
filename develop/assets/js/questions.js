168cedade7bf28da856b359a9682a773a0f9b43f
Monica Hill <monica.r.hill@gmail.com>
2020-12-24 16:27:56 -0500

    questions

--- develop/assets/js/questions.js
+++ develop/assets/js/questions.js
@@ -1,36 +1,58 @@
 // list of all questions, choices, and answers
 var questions = [
     {
-      title: "Commonly used data types DO NOT include:",
-      choices: ["strings", "booleans", "alerts", "numbers"],
-      answer: "alerts"
+      title: "____ can be used to represent values in your script that are likely to change.",
+      choices: ["Variables", "Strings", "Functions", "Parameters"],
+      answer: "Variables"
     },
     {
-      title: "The condition in an if / else statement is enclosed within ____.",
+      title: "To declare a function, you give it a name and then write the statements needed to achieve its task inside ________.",
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-      answer: "parentheses"
+      answer: "curly brackets"
     },
     {
-      title: "Arrays in JavaScript can be used to store ____.",
-      choices: [
-        "numbers and strings",
-        "other arrays",
-        "booleans",
-        "all of the above"
-      ],
-      answer: "all of the above"
+      title: "_____ store the code required to perform a specific task.",
+      choices: ["Variables", "Objects", "Methods", "Functions"],
+      answer: "Functions"
     },
     {
-      title:
-        "String values must be enclosed within ____ when being assigned to variables.",
-      choices: ["commas", "curly brackets", "quotes", "parentheses"],
-      answer: "quotes"
+      title: "When you call a function with parameters, you specify the values it should use in the parentheses that follow the name. The values are called _____.",
+      choices: ["data", "arguments", "tasks", "undefined"],
+      answer: "arguments"
     },
     {
-      title:
-        "A very useful tool used during development and debugging for printing content to the debugger is:",
-      choices: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+      title: "When the browser creates a model of a webpage, it not only creates a document object but, it also creates a new object for each element on the page. Together  these objects are described as the ____.",
+      choices: ["Direct Object Model", "Document Prototype Model", "Document Object Model", "Document Object Method"],
+      answer: "Document Object Model"
+    },
+
+    {
+     title: "If you need to run code a specific number of times, it's best to utilize a ____.",
+     choices: ["while loop", "do loop", "for loop"],
+     answer: "for loop"
+    },
+    {
+      title: "A special type of variable that stores lists of values is a(n) ____.",
+      choices: ["array", "unordered list", "ordered list", "list"],
+      answer: "array"
+     },
+     {
+      title: "Each item in an array is automatically given a number called an index that starts with ____.",
+      choices: ["1", "2", "i", "0"],
+      answer: "0"
+     },
+     {
+      title: "A strict equal to, (===), compares two values to check that both the data type and value are the same.",
+      choices: ["true", "false", "somewhat true", "somewhat false"],
+      answer: "true"
+     },
+     {
+      title: "____ is a quick expression that prints content to the debugger, which is very useful during the development process. ",
+      choices: ["console.log", "print", "document", "function"],
       answer: "console.log"
-    }
+     },
+
   ];
-  
+
+
+
